a1a8ee477ed944359f42f795b9d0fc755e4c4950
Dotty <dotty-dev@tuta.io>
2024-12-01 13:45:36 +0100

    removed autodarts login data and secrets from main files

---
+++ app_data/partials/autodarts-auth-data.example.php
@@ -0,0 +1,8 @@
+<?php
+
+$baseURL      = "https://login.autodarts.io";
+$realm        = "autodarts";
+$clientID     = "CLIENTID_FOR_YOUR_APP";
+$clientSecret = "CLIENT_SECRET_FOR_YOUR_APP";
+$userName     = "USERNAME_FOR_YOUR_ACCOUNT";
+$password     = "PASSWORD_FOR_YOUR_ACCOUNT";
